c97e89b6240a7222401f0c4b38146a272585b084
Diego <manuelchema999@gmail.com>
2025-06-24 19:00:45 -0500

    chore(setup): agrega el script setup.sh para inicializar el entorno del proyecto
    El script setup.sh automatizara la inicializacion de la configuracion del entorno del proyecto, actualizara pip, intalara las dependecias definidas en requirements.txt, instalara los hooks (commit-msg, pre-push) y dara permiso de ejecucion a estos hooks

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Actualizando pip ..."
+python3 -m pip install --upgrade pip
+
+echo "Instalando dependencias del proyecto ..."
+pip install -r requirements.txt
+
+#instalaremos los hooks que estan en hooks/ a .git/hooks/
+echo "Instalando hooks en .git/hooks/ ..."
+cp hooks/commit-msg .git/hooks/commit-msg
+cp hooks/pre-push .git/hooks/pre-push
+
+#le damos permiso de ejecucion a los hooks creados
+chmod +x .git/hooks/commit-msg
+chmod +x .git/hooks/pre-push
+
+echo "Hooks y dependencias instaladas correctamente"
